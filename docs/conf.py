680b43d9a93bad212525b382a77d04d175d26341
prem <prem.dubey@hashmapinc.com>
2019-05-24 15:15:12 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,9 @@ author = 'Hashmap, Inc'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
+# Setting master doc to index as contents does not exist
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 
