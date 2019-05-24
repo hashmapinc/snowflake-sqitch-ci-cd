bdc5fd2fcd871eae9550ba82f0950bba0e021d0b
prem0132 <prem.it.rits@gmail.com>
2019-05-24 15:29:10 +0530

    Adding html formatting

--- docs/conf.py
+++ docs/conf.py
@@ -44,12 +44,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# apply css widening of the too-narrow read-the-docs content section
+def setup(app):
+    app.add_stylesheet('wider-content.css')
